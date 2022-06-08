6beb14f76ec830e8be36224094b02804631a24aa
Harovinkin <kharovinkin@gmail.com>
2022-06-08 22:35:05 +0200

    inst npm react-icons

---
+++ find-country-flag/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled from "styled-components";
+import { Container } from './Container';
+
+export const Header = () => {
+  return (
+    <Container>Header</Container>
+  )
+}
