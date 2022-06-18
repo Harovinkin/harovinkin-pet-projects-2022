e4e51b0dde4bed4741bb3ee1652903518e49346b
Harovinkin <kharovinkin@gmail.com>
2022-06-18 20:59:30 +0200

    routing setup

--- find-country-flag/src/pages/HomePage.jsx
+++ find-country-flag/src/pages/HomePage.jsx
@@ -1,22 +1,29 @@
 import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 import { Controls } from '../components/Controls';
 import { ALL_COUNTRIES } from '../components/config';
 import { CountriesList } from '../components/CountriesList';
 import { Card } from '../components/Card';
-import { Main } from '../components/Main';
+
 export const HomePage = () => {
     const [countries, setCountries] = useState([]);
+
+    const navigate = useNavigate();
 
     useEffect(() => {
         axios.get(ALL_COUNTRIES).then(({ data }) => setCountries(data));
     }, []);
+
     return (
         <>
             <Controls />
             <CountriesList>
                 {countries.map((country) => {
+                    const goToDetails = () =>
+                        navigate(`country/${country.name}`);
+
                     const countryInfo = {
                         img: country.flags.png,
                         name: country.name,
@@ -37,7 +44,13 @@ export const HomePage = () => {
                         ],
                     };
 
-                    return <Card key={country.name} {...countryInfo} />;
+                    return (
+                        <Card
+                            key={country.name}
+                            onClick={goToDetails}
+                            {...countryInfo}
+                        />
+                    );
                 })}
             </CountriesList>
         </>
