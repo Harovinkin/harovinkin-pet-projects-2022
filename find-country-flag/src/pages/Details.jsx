d376d7805d82ea58184fde5bbf865116fbc40ba1
Harovinkin <kharovinkin@gmail.com>
2022-06-20 16:56:31 +0200

    add button module

--- find-country-flag/src/pages/Details.jsx
+++ find-country-flag/src/pages/Details.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { IoArrowBack } from 'react-icons/io5';
+import { searchByCountry } from '../components/config';
+import { Button } from '../components/Button';
 
 export const Details = () => {
     const { name } = useParams();
-    return <div>Details {name}</div>;
+    const navigate = useNavigate();
+    const [country, setCountry] = useState(null);
+
+    console.log(country);
+
+    useEffect(() => {
+        axios
+            .get(searchByCountry(name))
+            .then(({ data }) => setCountry(data[0]));
+    }, [name]);
+
+    const goBack = () => navigate(-1);
+    return (
+        <div>
+            <Button onClick={goBack}>
+                <IoArrowBack />
+                BACK
+            </Button>
+            Details {name}
+        </div>
+    );
 };
