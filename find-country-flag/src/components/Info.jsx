6aa953967357af37848ec47fe041c0eab8ea3efd
Harovinkin <kharovinkin@gmail.com>
2022-06-21 14:18:01 +0200

    styled info

--- find-country-flag/src/components/Info.jsx
+++ find-country-flag/src/components/Info.jsx
@@ -1,17 +1,91 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Wrapper = styled.section``;
+const Wrapper = styled.section`
+    margin-top: 3rem;
+    width: 100%;
+    display: grid;
+    grid-template-columns: 100%;
+    gap: 2rem;
 
-const InfoImage = styled.img``;
+    @media (min-width: 767px) {
+        grid-template-columns: minmax(100px, 400px) 1fr;
+        align-items: center;
+        gap: 5rem;
+    }
 
-const InfoTitle = styled.h2``;
+    @media (min-width: 1024px) {
+        grid-template-columns: minmax(400px, 600px) 1fr;
+    } ;
+`;
 
-const ListGroup = styled.div``;
+const InfoImage = styled.img`
+    display: block;
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+`;
 
-const List = styled.ul``;
+const InfoTitle = styled.h1`
+    margin: 0;
+    font-weight: var(--fw-normal);
+`;
 
-const ListItem = styled.li``;
+const ListGroup = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+
+    @media (min-width: 1024px) {
+        flex-direction: row;
+        gap: 4rem;
+    }
+`;
+
+const List = styled.ul`
+    list-style: none;
+    margin: 0;
+    padding: 0;
+`;
+
+const ListItem = styled.li`
+    line-height: 1.8;
+
+    & > b {
+        font-weight: var(--fw-bold);
+    }
+`;
+
+const Meta = styled.div`
+    margin-top: 3rem;
+    display: flex;
+    gap: 1.5rem;
+    flex-direction: column;
+    align-items: flex-start;
+
+    & > b {
+        font-weight: var(--fw-bold);
+    }
+
+    @media (min-width: 767px) {
+        flex-direction: row;
+        align-items: center;
+    } ;
+`;
+
+const TagGroup = styled.div`
+    display: flex;
+    gap: 0.5rem;
+    flex-wrap: wrap;
+`;
+
+const Tag = styled.span`
+    padding: 0 1rem;
+    background-color: var(--colors-ui-base);
+    box-shadow: var(--shadow);
+    line-height: 1.5;
+    cursor: pointer;
+`;
 
 export const Info = (props) => {
     const {
@@ -21,7 +95,7 @@ export const Info = (props) => {
         capital,
         population,
         region,
-        subREgion,
+        subregion,
         topLevelDomain,
         currencies = [],
         languages = [],
@@ -35,9 +109,58 @@ export const Info = (props) => {
             <div>
                 <InfoTitle>{name}</InfoTitle>
                 <ListGroup>
-                    <List></List>
-                    <List></List>
+                    <List>
+                        <ListItem>
+                            <b>Native name:</b> {nativeName}
+                        </ListItem>
+                        <ListItem>
+                            <b>Population:</b> {population}
+                        </ListItem>
+                        <ListItem>
+                            <b>Region:</b> {region}
+                        </ListItem>
+                        <ListItem>
+                            <b>Subregion:</b> {subregion}
+                        </ListItem>
+                        <ListItem>
+                            <b>Capital:</b> {capital}
+                        </ListItem>
+                    </List>
+
+                    <List>
+                        <ListItem>
+                            <b>Top Level Domain: </b>
+                            {topLevelDomain.map((d) => (
+                                <span key={d}>{d}</span>
+                            ))}
+                        </ListItem>
+                        <ListItem>
+                            <b>Currencies: </b>
+                            {currencies.map((c) => (
+                                <span key={c.code}>{c.name} </span>
+                            ))}
+                        </ListItem>
+                        <ListItem>
+                            <b>languages: </b>
+                            {languages.map((l) => (
+                                <span key={l.name}>{l.name}</span>
+                            ))}
+                        </ListItem>
+                    </List>
                 </ListGroup>
+
+                <Meta>
+                    <b>Border Countries: </b>
+                    {!borders.length ? (
+                        <span>There is no border countries</span>
+                    ) : (
+                        <TagGroup>
+                            {borders.map((b) => (
+                                <Tag key={b}>{b} </Tag>
+                            ))}
+                        </TagGroup>
+                    )}
+                </Meta>
             </div>
         </Wrapper>
     );
